cda3e43259c74c2c1bcb42b2efcb05d2e4291e5f
Vince Duarte <vinceduarte3@gmail.com>
2021-10-23 16:45:07 -0400

    login assets - added folders for each page - modified logic for browser router in app.js

--- sustainable-fisheries/src/App.js
+++ sustainable-fisheries/src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/Login/Login';
+import Forgot from './components/Forgot/Forgot';
+import SignUp from './components/SignUp/SignUp';
 import Home from './components/Home/Home';
 import Profile from './components/Profile/Profile';
+
 
 function App() {
   // TODO: 'setToken' is assigned a value but never used
@@ -10,7 +13,20 @@ function App() {
 
   if(!token) {
     return (
-        <Login />
+      <BrowserRouter>
+      <Login setToken={setToken} />
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/forgotpassword">
+          <Forgot />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+      </Switch>
+    </BrowserRouter>
     )
   }
   
