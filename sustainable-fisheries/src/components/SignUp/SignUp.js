0b0587aa18d5f4d6c3d7373bbdb118ad288e5051
jasonlvsk <jelusk21@gmail.com>
2021-10-25 14:00:25 -0400

    user interace added

---
+++ sustainable-fisheries/src/components/SignUp/SignUp.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './SignUp.css'
+
+
+export default function SignUp() {
+    return (
+       
+        <div className="login-wrapper center">
+        
+      
+            <form>
+            <header id="main-header">
+            <div class="container">
+            <h1>Sustainable Fisheries</h1>
+            </div>
+            
+            </header>
+            <h1 id="sign-in">Sign Up: </h1>
+                <div className="input">
+                    <label for="username">Username:</label>
+                    <input id="username" type="text" />
+                    <label for="username">Confirm Username:</label>
+                    <input id="username" type="text" />
+                    <label for="password">Password:</label>
+                    <input id="password" type="password" />
+                </div>
+                <div className="button">
+                    <button type="submit">Submit</button>
+                </div>
+                Already have an account? Log in:
+                <div className="button">
+                    <button type="submit">Sign Up</button>
+                </div>
+                Forgot Password?
+                <div className="button">
+                    <button type="submit">Forgot Password</button>
+                </div>
+            </form>
+
+            <footer id="main-footer">
+            <p>Copyright &copy; 2021 Sustainable Fisheries</p>
+            </footer>
+        </div>
+        
+       
+
+    );
+}
