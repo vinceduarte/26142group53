a7eda2316e09690ff6c03a758366a07bf90495d5
Vince Duarte <vinceduarte3@gmail.com>
2021-11-15 14:05:20 -0500

    signup and login APIs

--- sustainable-fisheries/server/server.js
+++ sustainable-fisheries/server/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
+require("dotenv").config({ path: "./config/config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
+const passport = require("passport");
+const users = require("./routes/api/users");
  
 app.listen(port, () => {
   // perform a database connection when server starts
@@ -17,3 +19,10 @@ app.listen(port, () => {
   });
   console.log(`Server is running on port: ${port}`);
 });
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
