098d6b0b66fb4974bf53faa1fb9c2c36219f2ae8
aadeshthirukonda <68292621+aadeshthirukonda@users.noreply.github.com>
2021-11-21 14:41:06 -0500

    Update users.js

--- sustainable-fisheries/server/routes/api/users.js
+++ sustainable-fisheries/server/routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const Post = require("../models/Post");
+
 // Load input validation
 const validateRegisterInput = require("../../validation/signup");
 const validateLoginInput = require("../../validation/login");
@@ -92,4 +94,59 @@ router.post("/login", (req, res) => {
     });
   });
 
-  module.exports = router;
+  //UPDATE
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      req.body.password = await bcrypt.hash(req.body.password, salt);
+    }
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(401).json("You cannot update");
+  }
+});
+
+//DELETE
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      try {
+        await Post.deleteMany({ username: user.username });
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted...");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } catch (err) {
+      res.status(404).json("User not found!");
+    }
+  } else {
+    res.status(401).json("You cannot delete");
+  }
+});
+
+//GET USER
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
