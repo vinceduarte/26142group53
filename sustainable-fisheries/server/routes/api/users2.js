f644ed47890bd3ae52b777ceefb36b10bf96712a
aadeshthirukonda <68292621+aadeshthirukonda@users.noreply.github.com>
2021-11-20 21:36:12 -0500

    Update users2.js

--- sustainable-fisheries/server/routes/api/users2.js
+++ sustainable-fisheries/server/routes/api/users2.js
@@ -1,84 +1,61 @@
-const express = require("express");
-const router = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");// Load input validation
-const validateRegisterInput = require("../../validation/signup");
-const validateLoginInput = require("../../validation/login");// Load User model
-const User = require("../../models/User");
+const router = require("express").Router();
+const User = require("../models/User");
+const Post = require("../models/Post");
+const bcrypt = require("bcrypt");
 
-// @route POST api/users/signup
-// @desc Register user
-// @access Public
-router.post("/signup", (req, res) => {
-    // Form validationconst { errors, isValid } = validateRegisterInput(req.body);// Check validation
-    if (!isValid) {
-      return res.status(400).json(errors);
-    }User.findOne({ email: req.body.email }).then(user => {
-      if (user) {
-        return res.status(400).json({ email: "Email already exists" });
-      } else {
-        const newUser = new User({
-          name: req.body.name,
-          email: req.body.email,
-          password: req.body.password
-        });// Hash password before saving in database
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
-            newUser
-              .save()
-              .then(user => res.json(user))
-              .catch(err => console.log(err));
-          });
-        });
+//UPDATE
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      req.body.password = await bcrypt.hash(req.body.password, salt);
+    }
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(401).json("You can update only your account!");
+  }
+});
+
+//DELETE
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      try {
+        await Post.deleteMany({ username: user.username });
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted...");
+      } catch (err) {
+        res.status(500).json(err);
       }
-    });
-  });
+    } catch (err) {
+      res.status(404).json("User not found!");
+    }
+  } else {
+    res.status(401).json("You can delete only your account!");
+  }
+});
 
-// @route POST api/users/login
-// @desc Login user and return JWT token
-// @access Public
-router.post("/login", (req, res) => {
-    // Form validationconst { errors, isValid } = validateLoginInput(req.body);// Check validation
-    if (!isValid) {
-      return res.status(400).json(errors);
-    }const email = req.body.email;
-    const password = req.body.password;// Find user by email
-    User.findOne({ email }).then(user => {
-      // Check if user exists
-      if (!user) {
-        return res.status(404).json({ emailnotfound: "Email not found" });
-      }// Check password
-      bcrypt.compare(password, user.password).then(isMatch => {
-        if (isMatch) {
-          // User matched
-          // Create JWT Payload
-          const payload = {
-            id: user.id,
-            name: user.name
-          };// Sign token
-          jwt.sign(
-            payload,
-            keys.secretOrKey,
-            {
-              expiresIn: 31556926 // 1 year in seconds
-            },
-            (err, token) => {
-              res.json({
-                success: true,
-                token: "Bearer " + token
-              });
-            }
-          );
-        } else {
-          return res
-            .status(400)
-            .json({ passwordincorrect: "Password incorrect" });
-        }
-      });
-    });
-  });
+//GET USER
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-  module.exports = router;
+module.exports = router;
